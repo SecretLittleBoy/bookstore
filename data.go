7497b9d09fc9402e0eb1bc7d69848b36e6a2939e
LYH <3210101621@zju.edu.cn>
2024-02-20 20:54:23 +0800

    update

--- data.go
+++ data.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"time"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"time"
+)
+
+const (
+	defaultShelfSize = 10
 )
 
 func NewDB(dsn string) (*gorm.DB, error) {
@@ -30,4 +37,42 @@ type Book struct {
 	ShelfID  int64
 	CreatAt  time.Time
 	UpdateAt time.Time
+}
+
+type bookstore struct {
+	db *gorm.DB
+}
+
+func (b *bookstore) CreateShelf(ctx context.Context, shelf Shelf) (*Shelf, error) {
+	if len(shelf.Theme) <= 0 {
+		return nil, errors.New("invalid theme")
+	}
+	if shelf.Size <= 0 {
+		shelf.Size = defaultShelfSize
+	}
+	v := Shelf{Theme: shelf.Theme, Size: shelf.Size, CreatAt: time.Now(), UpdateAt: time.Now()}
+	if err := b.db.Create(&v).Error; err != nil {
+		return nil, err
+	}
+	return &v, nil
+}
+
+func (b *bookstore) GetShelf(ctx context.Context, id int64) (*Shelf, error) {
+	var v Shelf
+	if err := b.db.WithContext(ctx).First(&v, id).Error; err != nil {
+		return nil, err
+	}
+	return &v, nil
+}
+
+func (b *bookstore) ListShelf(ctx context.Context) ([]*Shelf, error) {
+	var vs []*Shelf
+	if err := b.db.WithContext(ctx).Find(&vs).Error; err != nil {
+		return nil, err
+	}
+	return vs, nil
+}
+
+func (b *bookstore) DeleteShelf(ctx context.Context, id int64) error {
+	return b.db.WithContext(ctx).Delete(&Shelf{}, id).Error
 }
