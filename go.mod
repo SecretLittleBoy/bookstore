2ab2dd66c663e82b488b226c9e69f9266c6530d1
LYH <3210101621@zju.edu.cn>
2024-02-26 09:24:23 +0800

    update

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
+	golang.org/x/net v0.20.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20240213162025-012b6fc9bca9
 	google.golang.org/grpc v1.61.1
 	google.golang.org/protobuf v1.32.0
@@ -16,7 +17,6 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto v0.0.0-20240205150955-31a09d347014 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
 
-	 "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	useSamePort = false
 )
 
 func main() {
@@ -30,34 +37,67 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterBookstoreServer(s, &server)
-	go func() {
-		if err := s.Serve(l); err != nil {
-			fmt.Printf("failed to serve,err:%v\n", err)
+
+	if useSamePort {// 使用同一个端口
+		gwmux := runtime.NewServeMux() // 1. 创建gRPC-Gateway mux
+		dops := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+		if err := pb.RegisterBookstoreHandlerFromEndpoint(
+			context.Background(), gwmux, "127.0.0.1:8080", dops); err != nil {
+			fmt.Printf("RegisterBookstoreHandlerFromEndpoint failed, err:%v\n", err)
 			return
 		}
-	}()
 
-	conn, err := grpc.DialContext(context.Background(), "localhost:8080",
-		grpc.WithBlock(),grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		fmt.Printf("grpc.DialContext failed,err:%v\n", err)
-		return
-	}
-	defer conn.Close()
+		mux := http.NewServeMux() // 2. 新建HTTP mux
+		mux.Handle("/", gwmux)
 
-	gwmux := runtime.NewServeMux()
-	err = pb.RegisterBookstoreHandler(context.Background(), gwmux, conn)
-	if err != nil {
-		fmt.Printf("pb.RegisterBookstoreHandler failed,err:%v\n", err)
-		return
+		gwServer := &http.Server{ // 3. 定义HTTP Server
+			Addr:    "127.0.0.1:8080",
+			Handler: grpcHandlerFunc(s, mux),
+		}
+		// 4. 启动服务
+		fmt.Println("serving on 127.0.0.1:8080...")
+		gwServer.Serve(l)
+	} else {// 使用不同的端口
+		go func() {
+			if err := s.Serve(l); err != nil {
+				fmt.Printf("failed to serve,err:%v\n", err)
+				return
+			}
+		}()
+
+		conn, err := grpc.DialContext(context.Background(), "localhost:8080",
+			grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			fmt.Printf("grpc.DialContext failed,err:%v\n", err)
+			return
+		}
+		defer conn.Close()
+
+		gwmux := runtime.NewServeMux()
+		err = pb.RegisterBookstoreHandler(context.Background(), gwmux, conn)
+		if err != nil {
+			fmt.Printf("pb.RegisterBookstoreHandler failed,err:%v\n", err)
+			return
+		}
+		gwServer := &http.Server{
+			Addr:    ":8081",
+			Handler: gwmux,
+		}
+		fmt.Println("grpc-Gateway serve on :8081...")
+		if err := gwServer.ListenAndServe(); err != nil {
+			fmt.Printf("gwServer.ListenAndServe failed,err:%v\n", err)
+			return
+		}
 	}
-	gwServer := &http.Server{
-		Addr:    ":8081",
-		Handler: gwmux,
-	}
-	fmt.Println("grpc-Gateway serve on :8081...")
-	if err := gwServer.ListenAndServe(); err != nil {
-		fmt.Printf("gwServer.ListenAndServe failed,err:%v\n", err)
-		return
-	}
+}
+
+// grpcHandlerFunc 将gRPC请求和HTTP请求分别调用不同的handler处理
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
